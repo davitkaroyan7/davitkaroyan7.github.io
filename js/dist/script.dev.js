ea980ba8167ca2b445b0bcd0c53b40e0fd74de99
davitkaroyan <69844149+davitkaroyan7@users.noreply.github.com>
2020-08-18 16:42:24 +0500

    Add files via upload

---
+++ js/dist/script.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded', function () {
+  var menu = document.querySelector('.menu'),
+      menuItem = document.querySelectorAll('.menu_item'),
+      hamburger = document.querySelector('.hamburger');
+  hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('menu_active');
+  });
+  menuItem.forEach(function (item) {
+    item.addEventListener('click', function () {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu_active');
+    });
+  });
+});
+//# sourceMappingURL=script.dev.js.map
